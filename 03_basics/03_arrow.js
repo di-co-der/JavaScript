930f46d458212022c44f29886a12676d4d8b6bb9
di-co-der <raghavdivyansh9@gmail.com>
2024-07-27 09:26:47 +0530

    update 03_arrow.js file

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -1,10 +1,13 @@
+// this is not used in arrow function 
+//No arguments object in arrow functions
+// this talk about current context 
 const user = {
     username: "hitesh",
     price: 999,
 
     welcomeMessage: function() {
         console.log(`${this.username} , welcome to website`);
-        console.log(this);
+        // console.log(this);
     }
 
 }
@@ -13,10 +16,15 @@ const user = {
 // user.username = "sam"
 // user.welcomeMessage()
 
+
+//In browser  console.log(this); gives window object and in node envioronment it gives empty object.
+// because in browser the global object is window object 
 // console.log(this);
+
 
 // function chai(){
 //     let username = "hitesh"
+//     console.log(this)
 //     console.log(this.username);
 // }
 
@@ -33,7 +41,7 @@ const chai =  () => {
 }
 
 
-// chai()
+chai()
 
 // const addTwo = (num1, num2) => {
 //     return num1 + num2
@@ -41,14 +49,16 @@ const chai =  () => {
 
 // const addTwo = (num1, num2) =>  num1 + num2
 
-// const addTwo = (num1, num2) => ( num1 + num2 )
+// const addTwo = (num1, num2) => ( num1 + num2 ) //without return keyword, it uses in react.
 
-const addTwo = (num1, num2) => ({username: "hitesh"})
+// const addTwo = (num1, num2) => ({username: "hitesh"})
 
 
-console.log(addTwo(3, 4))
+// console.log(addTwo(3, 4))
 
 
 // const myArray = [2, 5, 3, 7, 8]
 
 // myArray.forEach()
+
+// refer :- https://www.freecodecamp.org/news/the-difference-between-arrow-functions-and-normal-functions/
