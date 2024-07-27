8cb3d1831f0e9d0335cd4572a9fb475c75c20330
di-co-der <raghavdivyansh9@gmail.com>
2024-07-27 09:33:21 +0530

    update 04_control_flow file

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -4,9 +4,10 @@
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
-})();
+})(); //dont forget to semicolon
 
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
 } )('hitesh')
 
+// global scope ke pollution se bachne ke liye iife ka use krte hain
