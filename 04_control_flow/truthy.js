93f2674c2500e1dd08de5ca19ba57e170dd52a34
di-co-der <raghavdivyansh9@gmail.com>
2024-07-28 15:01:37 +0530

    update truthy.js

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -19,17 +19,25 @@ if (userEmail) {
 
 const emptyObj = {}
 
-if (Object.keys(emptyObj).length === 0) {
+if (Object.keys(emptyObj).length === 0) { //Object.keys(emptyObj) gives you array of keys
     console.log("Object is empty");
 }
 
+
+// false == 0 //output true
+// false == '' //output true
+// 0 == '' //output true
+
 // Nullish Coalescing Operator (??): null undefined
 
+//it is used when you call from data base and if database return null then we change accordingly
+//to handle error
 let val1;
-// val1 = 5 ?? 10
+// val1 = 5 ?? 10 //phli not null/unefined value jayegi
 // val1 = null ?? 10
 // val1 = undefined ?? 15
-val1 = null ?? 10 ?? 20
+// val1 = null ?? 10 ?? 20
+val1 = null ?? undefined
 
 
 
