11b81a06ee5c26fcef5be2a6a13f3e725e506240
di-co-der <raghavdivyansh9@gmail.com>
2024-07-28 14:43:09 +0530

    update one.js

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -23,7 +23,7 @@ const temperature = 41
 
 // const balance = 1000
 
-// if (balance > 500) console.log("test"),console.log("test2");
+// if (balance > 500) console.log("test"),console.log("test2"); // comma same scope btata hai pr not a good practice.
 
 // if (balance < 500) {
 //     console.log("less than 500");
