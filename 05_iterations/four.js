b2440999e06d462a986ed2336e55951effa762db
di-co-der <raghavdivyansh9@gmail.com>
2024-07-28 16:04:10 +0530

    update four.js

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -9,9 +9,13 @@ for (const key in myObject) {
     //console.log(`${key} shortcut is for ${myObject[key]}`);
 }
 
+for(const [key, value] in myObject){
+    console.log(`key is ${key} and value is ${value}`) //not valid
+}
+
 const programming = ["js", "rb", "py", "java", "cpp"]
 
-for (const key in programming) {
+for (const key in programming) {   // it takes index in the key
     //console.log(programming[key]);
 }
 
@@ -24,3 +28,4 @@ for (const key in programming) {
 // for (const key in map) {
 //     console.log(key);
 // }
+//map is not iterable
